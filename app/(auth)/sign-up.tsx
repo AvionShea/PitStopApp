aab9148c08d474a3b007c81836388085b7f6ebd5
AvionShea <avionsheadesigns@gmail.com>
2024-08-18 22:16:06 -0400

    basic success verification working

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,13 +2,17 @@ import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
 import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
+import { useSignUp } from "@clerk/clerk-expo";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Link } from "expo-router";
 import { useState } from "react";
 import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import ReactNativeModal from "react-native-modal";
 
 
 const SignUp = () => {
+    const { isLoaded, signUp, setActive } = useSignUp();
+
     const [form, setForm] = useState({
         firstName: "",
         lastName: "",
@@ -17,7 +21,56 @@ const SignUp = () => {
         password: "",
     });
 
-    const onSignUpPress = async () => { };
+    const [verification, setVerification] = useState({
+        state: "success",
+        error: "",
+        code: "",
+    });
+
+    const onSignUpPress = async () => {
+        if (!isLoaded) {
+            return
+        };
+
+        try {
+            await signUp.create({
+                emailAddress: form.email,
+                password: form.password,
+            });
+
+            await signUp.prepareEmailAddressVerification({ strategy: 'email_code' });
+
+            setVerification({
+                ...verification,
+
+                state: "pending",
+            });
+        } catch (err: any) {
+            console.error(JSON.stringify(err, null, 2))
+        };
+    };
+
+    const onPressVerify = async () => {
+        if (!isLoaded) return;
+
+        try {
+            const completeSignUp = await signUp.attemptEmailAddressVerification({
+                code: verification.code,
+            });
+
+            if (completeSignUp.status === 'complete') {
+
+                //TODO: Create a database user
+
+                await setActive({ session: completeSignUp.createdSessionId });
+                setVerification({ ...verification, state: "success" });
+            } else {
+                setVerification({ ...verification, error: "Verification failed.", state: "failed" });
+            }
+        } catch (err: any) {
+            setVerification({ ...verification, error: err.errors[0].longMessage, state: "failed" });
+        };
+    };
 
     const [showPassword, setShowPassword] = useState(false);
     const toggleShowPassword = () => {
@@ -100,7 +153,11 @@ const SignUp = () => {
                     </Link>
                 </View>
 
-                {/*VERIFICATION MODAL */}
+                <ReactNativeModal isVisible={verification.state === "success"}>
+                    <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+                        <Image source={images.check} className="w-[110px] h-[110px] mx-auto my-5" />
+                    </View>
+                </ReactNativeModal>
 
             </View>
         </ScrollView>
